8a9a3b825f275b7f596a2c6bc428ddddcf13bd2a
William Studer <bill.studer@sent.com>
2020-07-01 11:51:55 -0500

    using properties file instead of props

--- README.md
+++ README.md
@@ -1,13 +1,23 @@
 # tda-client-example
 Simple Java & Maven example of TDA Client
 
+`git clone https://github.com/studerw/tda-client-example.git`
+
+Add your client id (customer key) and refresh token to the `src/main/resources/tda-api.properties` file.
+Make sure you then add this file to `.gitignore` so you don't accidentally check in sensitive data
+to a public repository.
+
+`echo 'src/main/resources/tda-api.properties' >> .gitignore` 
+
 ## To Build
 
 `mvn clean install`
 
 ## To Run
+Run the `MainExample` class in your IDE. Or from the command line:
 
-* Add the client id and refresh token in the MainExample class.
+`mvn exec:java -Dexec.cleanupDaemonThreads=false`
 
-Run `mvn exec:java -Dexec.cleanupDaemonThreads=false`
+On Windows, you'll probably have to add quotes to the `-Dexec...`.
+
 
--- src/main/java/org/example/MainExample.java
+++ src/main/java/org/example/MainExample.java
@@ -2,11 +2,8 @@ package org.example;
 
 import com.studerw.tda.client.HttpTdaClient;
 import com.studerw.tda.client.TdaClient;
-import com.studerw.tda.model.account.Order;
 import com.studerw.tda.model.quote.EquityQuote;
 import com.studerw.tda.model.quote.Quote;
-import java.util.List;
-import java.util.Properties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,19 +11,12 @@ public class MainExample {
   private static final Logger LOGGER = LoggerFactory.getLogger(MainExample.class);
 
   public static void main(String[] args) {
-    Properties props = new Properties();
-    props.setProperty("tda.client_id", "...");
-    props.setProperty("tda.token.refresh", "...");
+    TdaClient client = new HttpTdaClient();
 
-    TdaClient tdaClient = new HttpTdaClient(props);
-
-    Quote quote = tdaClient.fetchQuote("MSFT");
+    Quote quote = client.fetchQuote("MSFT");
     EquityQuote equityQuote = (EquityQuote) quote;
     LOGGER.debug("Current price of MSFT: {}", equityQuote.getAskPrice());
-
-    final List<Order> orders = tdaClient.fetchOrders();
-    orders.forEach(order -> LOGGER.debug("{}", order));
-
+    
     System.exit(0);
   }
 
