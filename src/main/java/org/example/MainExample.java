85fc5f1bc22c92f470ba3f964275d07a2bb2b27e
William Studer <bill.studer@sent.com>
2021-02-09 19:31:58 -0500

    [option_chain_test_40] - try OSTK from issue 40

--- src/main/java/org/example/MainExample.java
+++ src/main/java/org/example/MainExample.java
@@ -2,8 +2,7 @@ package org.example;
 
 import com.studerw.tda.client.HttpTdaClient;
 import com.studerw.tda.client.TdaClient;
-import com.studerw.tda.model.quote.EquityQuote;
-import com.studerw.tda.model.quote.Quote;
+import com.studerw.tda.model.option.OptionChain;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,9 +12,8 @@ public class MainExample {
   public static void main(String[] args) {
     TdaClient client = new HttpTdaClient();
 
-    Quote quote = client.fetchQuote("MSFT");
-    EquityQuote equityQuote = (EquityQuote) quote;
-    LOGGER.debug("Current price of MSFT: {}", equityQuote.getAskPrice());
+    final OptionChain ostk = client.getOptionChain("OSTK");
+    LOGGER.debug("{}", ostk);
 
     System.exit(0);
   }
